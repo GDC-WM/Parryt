b820cd2ac9333f94e458f08d688e2569a5ea3ac6
aaronamk <amklein@email.wm.edu>
2020-02-08 21:37:17 -0500

    Cleaned up actor and master_logic classes.

--- include/actor.h
+++ include/actor.h
@@ -13,16 +13,9 @@ class Actor {
          */
         double x, y, width, height, xSpeed, ySpeed;
         int orientation; // orientation of the actor
-        ActorType type; // type of actor
 
     public:
         Actor(ActorType type, double x, double y, double width, double height);
-
-        virtual void update(float delta);
-
-        ActorType getType(void) { return this->type; };
-
-        void setType(ActorType type) { this->type = type; };
 
         void setX(double x) { this->x = x; };
 
@@ -34,15 +27,15 @@ class Actor {
 
         void setPos(double x, double y) { this->x = x; this->y = y; };
 
-        double getCenterX(void) { return (this->x + this->width / 2); };
-
-        double getCenterY(void) { return (this->y + this->height / 2); };
-
         double getWidth(void) { return this->width; };
+
+		void setWidth(double w) { this->width = w; };
 
         double getHeight(void) { return this->height; };
 
-        void setWidthHeight(double width, double height) { this->width = width; this->height = height; };
+		void setHeight(double h) { this->height = h; };
+
+		void setDimensions(double width, double height) { this->width = width; this->height = height; };
 
         double getXSpeed(void) { return this->xSpeed; };
 
@@ -106,6 +99,16 @@ class Actor {
         bool collidesCircle(Actor &a, int range);
 
         bool liesInsideSquare(Actor &a);
+
+        virtual void update(float delta);
+
+		/*
+		 * Draw actor.
+		 *
+		 * window -- window to draw itself on
+		 */
+		virtual void draw(std::shared_ptr<sf::RenderWindow> window) = 0;
+
 };
 
 #endif
--- include/master_logic.h
+++ include/master_logic.h
@@ -6,7 +6,6 @@
 
 #include "actor.h"
 #include "room.h"
-#include "exit.h"
 
 class MasterView;
 
@@ -14,41 +13,55 @@ class MasterView;
 * Main logic class that controls everything.
 */
 class MasterLogic {
-    private:
-        std::shared_ptr<MasterView> view;
-        std::list<std::shared_ptr<Room>> roomList;
-        std::list<std::shared_ptr<Room>>::iterator currentRoom;
-        std::list<std::shared_ptr<Actor>> actorList;
+	private:
+		std::shared_ptr<MasterView> view;
+		std::list<std::shared_ptr<Room>> roomList;
+		std::list<std::shared_ptr<Room>>::iterator currentRoom;
+		std::list<std::shared_ptr<Actor>> actorList;
 
-        std::shared_ptr<Exit> currentExit;
+	public:
+		bool paused = true;
 
-		float delta;
+		/*
+		 * Main method
+		 */
+		MasterLogic(void) { };
 
-    public:
-        bool paused = true;
+		/*
+		 *
+		 */
+		void init(std::shared_ptr<MasterView> mv);
 
-        MasterLogic(void) { };
 
-        void init(std::shared_ptr<MasterView> mv);
+		/*
+		 * Start main menu
+		 */
+		void startMenu(void);
 
-        void startMenu(void);
+		/*
+		 * Start a demo of gameplay
+		 */
+		void startDemo(void);
 
-        void startTutorial(void);
+		/*
+		 *
+		 */
+		std::shared_ptr<Room> getCurrentRoom(void) { return *(this->currentRoom); };
 
-        void startDemo(void);
+		/*
+		 *
+		 */
+		void checkCollisions(void);
 
-        void update(float delta);
+		/*
+		 * Resets master logic
+		 */
+		void reset(void);
 
-        std::shared_ptr<Room> getCurrentRoom(void) { return *(this->currentRoom); };
-
-        void checkCollisions(void);
-
-        int getNightCount(void) { return nightCount; };
-
-        void setNightCount(int num) { this->nightCount = num; };
-
-        // Resets all the lists in the game for when the player wins or loses
-        void reset(void);
+		/*
+		 *
+		 */
+		void update(float dt);
 };
 
 #endif
