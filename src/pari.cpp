b380e31fc233bc0b3e167d9d0708344f2fe7933d
David Solinsky <malynvirth2@gmail.com>
2020-03-09 14:52:57 -0400

    Gravity Brainstorming

--- src/pari.cpp
+++ src/pari.cpp
@@ -7,9 +7,10 @@
 Pari::Pari(double x, double y) : Character(x, y, 64, 64, 10, 180, 200) {
 	this->shape.setFillColor(sf::Color::Green);
 
+
 //placeholder to link base png as Pari
 //        sf::Texture texture;
-//        if(!texture.loadFromFile("/Users/davids/Documents/ParryT/WM-GDC-Parryt/src/Parryt-2.png")){std::cout<<"daaaang"<<std::endl; system("pause");}
+//        if(!texture.loadFromFile("/Users/*/./ParryT/WM-GDC-Parryt/src/Parryt-2.png")){std::cout<<"daaaang"<<std::endl; system("pause");}
 //        sf::Sprite sprite;
 //        sprite.setTexture(texture);
 //        sprite.setTextureRect(sf::IntRect(0, 0, 64, 64));
@@ -19,6 +20,9 @@ sf::Shape &Pari::getShape(void) {
 	this->shape.setSize(sf::Vector2f(this->width, this->height));
 	return (this->shape);
 }
+
+
+
 
 
 
