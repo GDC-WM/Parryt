a8de8caaf746a499a158536ed6b8bbef7e1025d8
David Solinsky <malynvirth2@gmail.com>
2020-03-09 11:22:59 -0400

    added_movement

--- src/character.cpp
+++ src/character.cpp
@@ -8,6 +8,7 @@
 #include "actor.h"
 #include "character.h"
 
+
 Character::Character(double x, double y, double width, double height, double mass, double maxSpeed, int maxHealth)
     : Actor(x, y, width, height) {
     this->mass = mass;
@@ -28,10 +29,28 @@ void Character::heal(int healAmount) {
 }
 
 void Character::move(void) {
+    // mouse
+    if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {/* slice with blade */ }
+    // keyboard
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) { /*pan the camera up*/}
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) { xSpeed -= 0.5; }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) &&
+        (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))) { /*jump down through a platform*/}
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) { xSpeed += 0.5; }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) { ySpeed -= 5; }
+
+    if (xSpeed > 0) { xSpeed -= 0.1; }
+    else if (xSpeed < 0) { xSpeed += 0.1; }
+    if (ySpeed < 0) { ySpeed -= 0.4; }
+    else if (ySpeed > 0) { ySpeed -= 0.2; }
+
+    /*if(!collidesSquare()){ ySpeed += 0.2;}
+     * else if(collidesSquare()){ySpeed=0;}*/
 
 }
 
 void Character::update(const float &dt) {
     Actor::update(dt);
+    move();
 }
 
--- src/pari.cpp
+++ src/pari.cpp
@@ -3,11 +3,22 @@
 #include "pari.h"
 #include <iostream>
 
+
 Pari::Pari(double x, double y) : Character(x, y, 64, 64, 10, 180, 200) {
 	this->shape.setFillColor(sf::Color::Green);
+
+//placeholder to link base png as Pari
+//        sf::Texture texture;
+//        if(!texture.loadFromFile("/Users/davids/Documents/ParryT/WM-GDC-Parryt/src/Parryt-2.png")){std::cout<<"daaaang"<<std::endl; system("pause");}
+//        sf::Sprite sprite;
+//        sprite.setTexture(texture);
+//        sprite.setTextureRect(sf::IntRect(0, 0, 64, 64));
 }
 
 sf::Shape &Pari::getShape(void) {
 	this->shape.setSize(sf::Vector2f(this->width, this->height));
 	return (this->shape);
 }
+
+
+
