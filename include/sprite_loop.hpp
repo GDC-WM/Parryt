7c6f0a4de51fc3e5cb54db3ef3146e7e3b6f33f9
aaronamk <amklein@email.wm.edu>
2020-11-03 16:51:13 -0500

    Set the SpriteLoop class.

--- include/sprite_loop.hpp
+++ include/sprite_loop.hpp
@@ -1,28 +1,43 @@
 #ifndef SPRITE_LOOP_HPP
 #define SPRITE_LOOP_HPP
 
+
 #include <SFML/Graphics.hpp>
 
-class SpriteReader {
-		public:
-			SpriteReader(std::string spriteSheet);
 
-			/**
-			 * Step to the next image in the loop
-			 *
-			 * @return returns the next image
-			 */
-			sf::Sprite step(void);
+/**
+ * Facilitates creating a sequence of sprites from a spritesheet.
+ */
+class SpriteLoop {
+	public:
+		/**
+		 * @param spriteSheet filename
+		 * @param divisionSize the vector size of a sprite in pixels (x,y) on the sprite sheet
+		 * @param startFrame the number of the first frame on the sprite sheet in the animation
+		 * @param frames the number of frames in the sequence
+		 * @param refreshRate the rate that the animation runs at (frames per second)
+		 */
+		SpriteLoop(std::string spriteSheet, sf::Vector2f divisionSize, int startFrame, int frames, float refreshRate);
 
-			/**
-			 * @param sprite is the sprite we wish to change position
-			 * @param x is the x coordinate where we wish to move
-			 * @param y is the y coordinate where we wish to move
-			 */
-			void setImgPosition(sf::Sprite sprite, int x, int y);
+		/**
+		 * @return the current sprite
+		 */
+		sf::Sprite &getSprite(void);
 
-		private:
-				sf::Texture spriteSheet;
+		/**
+		 * Reset the animation to the first frame in the sequence.
+		 */
+		void reset(void);
+
+
+	private:
+		sf::Texture spriteSheet;
+		sf::Sprite sprite;
+		int startFrame;
+		int frames;
+		float refreshRate;
+
 };
+
 
 #endif
