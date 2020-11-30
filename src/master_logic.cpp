e0439a4d271bb67a02f3f0dd006466747e5d28f4
Alan Zheng <alanzheng98@yahoo.com>
2020-11-30 13:48:15 -0500

    Level design (#60)
    * added background and did some level design LMAO
    
    * changed paris starting locaycay
    
    * changed platform color to light brownn

--- src/master_logic.cpp
+++ src/master_logic.cpp
@@ -25,29 +25,25 @@ void MasterLogic::startDemo(void) {
 	this->roomList.push_front(std::make_shared<Room>());
 	this->currentRoom = this->roomList.begin();
 
-	std::shared_ptr<Platform> ground = std::make_shared<Platform>(b2Vec2(0, 0), 200);
+	std::shared_ptr<Platform> ground = std::make_shared<Platform>(b2Vec2(0, 0), 50);
 	this->getCurrentRoom()->addActor(ground);
 
 	// Add pari
-	std::shared_ptr<Pari> pari = std::make_shared<Pari>(b2Vec2(10,2));
+	std::shared_ptr<Pari> pari = std::make_shared<Pari>(b2Vec2(-13,2));
 	this->getCurrentRoom()->addActor(pari);
 	this->view->addView(pari);
 
-	// Add cannon
-	std::shared_ptr<Cannon> cannon = std::make_shared<Cannon>(b2Vec2(1,1));
+	//Add cannon
+	std::shared_ptr<Cannon> cannon = std::make_shared<Cannon>(b2Vec2(13.5,8.3));
 	this->getCurrentRoom()->addActor(cannon);
 	this->view->addView(cannon);
 
 	// Add Platform
-	std::shared_ptr<Platform> platform1 = std::make_shared<Platform>(b2Vec2(5, 10), 20);
+	std::shared_ptr<Platform> platform1 = std::make_shared<Platform>(b2Vec2(31.9, 7.5), 20);
 	this->getCurrentRoom()->addActor(platform1);
 
-	// Add Another Platform
-	std::shared_ptr<Platform> platform2 = std::make_shared<Platform>(b2Vec2(200, 75), 20);
-	this->getCurrentRoom()->addActor(platform2);
-
 	// Add Wall
-	std::shared_ptr<Wall> wall1 = std::make_shared<Wall>(b2Vec2(10, 2), 4);
+	std::shared_ptr<Wall> wall1 = std::make_shared<Wall>(b2Vec2(12, 3.8), 3.7);
 	this->getCurrentRoom()->addActor(wall1);
 }
 
