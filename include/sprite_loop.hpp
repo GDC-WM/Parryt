35bba81353ae3ee1fcc8c3c1adca25988ddf6401
aaronamk <amklein@email.wm.edu>
2020-12-05 10:13:43 -0500

    Cleaned SpriteLoop class.

--- include/sprite_loop.hpp
+++ include/sprite_loop.hpp
@@ -6,9 +6,7 @@
 #include <chrono>
 
 
-struct Loop {
-	int start, frames, frameTime;
-};
+struct Loop { int start, frames, frameTime; };
 
 
 /**
@@ -19,7 +17,6 @@ class SpriteLoop {
 		/**
 		 * @param spriteSheet filename
 		 * @param spriteSize the vector size of a sprite in pixels (x,y) on the sprite sheet
-		 * @param frameTime the time between each frame of the animation
 		 */
 		SpriteLoop(const std::string &spriteSheet, const sf::Vector2i &spriteSize);
 
@@ -30,8 +27,19 @@ class SpriteLoop {
 
 		/**
 		 * Set the loop within the sprite sheet.
+		 *
+		 * @param loop new loop
 		 */
 		void setLoop(const Loop &loop) { this->loop = loop; };
+
+		/**
+		 * Set the loop within the sprite sheet.
+		 *
+		 * @param start the frame number of the first frame in the sequence
+		 * @param frames the number of frames in the sequence
+		 * @param frameTime the time between each frame of the animation
+		 */
+		void setLoop(const int &start, const int &frames, const int &frameTime) { this->loop = {start, frames, frameTime}; };
 
 		/**
 		 * Reset the animation to the first frame in the sequence.
@@ -43,13 +51,14 @@ class SpriteLoop {
 		sf::Texture texture;
 		sf::Sprite sprite;
 		sf::IntRect spriteRect; // pixels
-		sf::Vector2i sheetSize;  // rows/columns
+		sf::Vector2i sheetSize; // rows/columns
 		Loop loop;
-		int startFrame;
 		std::chrono::system_clock::time_point startTime;
 
 		/**
 		 * Set the frame within the frame loop.
+		 *
+		 * @param frame number ordered left to right, top to bottom
 		 */
 		void setFrame(const int &frame);
 };
