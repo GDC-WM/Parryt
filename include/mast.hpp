334ae01bc9ddfd7321f631cf42932f38abae52ee
Smeppitty <46271620+Smeppitty@users.noreply.github.com>
2020-12-07 00:04:49 -0500

    Mast (#66)
    * merge
    
    * mast realigned
    
    * realigned sprites
    
    * mast yey
    
    * premerge commit
    
    * MAST PLATFORMS WOW

---
+++ include/mast.hpp
@@ -0,0 +1,28 @@
+#ifndef MAST_HPP
+#define MAST_HPP
+
+
+#include <box2d/box2d.h>
+
+#include "actor.hpp"
+#include "platform.hpp"
+
+
+class Mast : public Actor {
+	public:
+		static constexpr float height = 0.1;
+
+		Mast(b2Vec2 position, float height);
+
+		virtual void draw(std::shared_ptr<sf::RenderWindow> window);
+
+	private:
+		b2PolygonShape shape;
+        b2Vec2 coords;
+        sf::RectangleShape drawable;
+		sf::Texture texture;
+        sf::Sprite sprite;
+	};
+
+
+#endif
