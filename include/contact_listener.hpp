45a87027327d7318a1c8f7475fe7d5e82d2c148c
aaronamk <amklein@email.wm.edu>
2021-01-31 10:58:23 -0500

    Improved contact listener class.

--- include/contact_listener.hpp
+++ include/contact_listener.hpp
@@ -1,8 +1,11 @@
+#ifndef CONTACT_LISTENER_HPP
+#define CONTACT_LISTENER_HPP
+
+
 #include <list>
 #include <memory>
 #include <box2d/b2_world_callbacks.h>
 
-#include "contact_listener.hpp"
 #include "actor.hpp"
 #include "exit.hpp"
 #include "platform.hpp"
@@ -14,6 +17,12 @@
 #include "mast.hpp"
 
 
-void ContactListener::BeginContact(b2Contact *contact) {
-	std::reinterpret_cast<uintptr_t>(contact->GetFixtureA()->GetBody()->GetUserData());
-}
+class ContactListener : public b2ContactListener {
+	public:
+		void BeginContact(b2Contact *contact);
+
+		void EndContact(b2Contact *contact);
+};
+
+
+#endif
