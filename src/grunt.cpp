2fac38ab54a1468fd2ae8ba0e27843af34b88265
aaronamk <amklein@email.wm.edu>
2021-02-18 01:17:42 -0500

    Formatting.

--- src/grunt.cpp
+++ src/grunt.cpp
@@ -8,10 +8,10 @@
 
 Grunt::Grunt(b2Vec2 position) : Character(position) {
 	this->allegiance = Allegiance::PIRATE;
-	
+
 	bodyDef.fixedRotation = true;
 	this->bodyDef.type = b2_dynamicBody;
-	
+
 	// fix shape to body
 	this->shape.SetAsBox(this->WIDTH, this->HEIGHT);
 	this->fixtureDef.shape = &this->shape;
@@ -22,7 +22,7 @@ Grunt::Grunt(b2Vec2 position) : Character(position) {
 	texture.loadFromFile("../resources/pirate.png");
 	this->sprite = sf::Sprite(texture, sf::IntRect(0,0,64,64));
 	sprite.setScale(0.08,0.08); //hardcoding BAD
-		
+
 	//set old drawable
 	this->drawable.setOrigin(this->WIDTH, this->HEIGHT);
 	this->drawable.setFillColor(sf::Color::Red);
@@ -38,12 +38,12 @@ void Grunt::shoot() {
 void Grunt::draw(std::shared_ptr<sf::RenderWindow> window) {
 	// old drawable
 	this->drawable.setPosition(this->getBody()->GetPosition().x,
-							  -this->getBody()->GetPosition().y);
+	                          -this->getBody()->GetPosition().y);
 	//window->draw(drawable);
 
 	// For now we just draw the static image.
 	this->sprite.setPosition(this->getBody()->GetPosition().x,
-							-this->getBody()->GetPosition().y);
-	
+	                        -this->getBody()->GetPosition().y);
+
 	window->draw(this->sprite);
 }
