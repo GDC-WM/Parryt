ad4316428f3b9f35227ef3f6309375c2dccfc7e1
rlatosky <latoskyr@gmail.com>
2021-02-17 22:59:54 -0500

    increased acceleration

--- src/pari.cpp
+++ src/pari.cpp
@@ -10,7 +10,7 @@
 Pari::Pari(b2Vec2 position) : Character(position) {
 	this->allegiance = Allegiance::PARROT;
 
-	this->acceleration = 360;
+	this->acceleration = 380;
 	this->deceleration = 250;
 	this->jumpImpulse = 235;
 	this->maxSpeed = 15;
