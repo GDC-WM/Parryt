45e06b20feb821416a920c64c604b28f6d27e6f5
aaronamk <amklein@email.wm.edu>
2021-02-17 23:09:12 -0500

    More commenting.

--- include/character.hpp
+++ include/character.hpp
@@ -14,24 +14,54 @@ class Character : public Actor, public std::enable_shared_from_this<Character> {
 public:
 	Character(b2Vec2 position);
 
-	virtual void damage(float d);
+	/**
+	 * @param damage Amount of damage to do
+	 */
+	virtual void damage(float damage);
 
+	/**
+	 * @param healAmount Amount to heal
+	 */
 	void heal(int healAmount);
 
+	/**
+	 * @param health new health
+	 */
 	void setHealth(double health) { this->health = health; };
 
+	/**
+	 * @return Current health
+	 */
 	const int getHealth(void) const { return this->health; };
 
+	/**
+	 * @param health Current max health
+	 */
 	void setMaxHealth(double health) { this->maxHealth = health; };
 
+	/**
+	 * @return Current max health
+	 */
 	const int getMaxHealth(void) const { return this->maxHealth; };
 
+	/**
+	 * Make the character jump
+	 */
 	virtual bool jump(void);
 
+	/**
+	 * Make the character go left
+	 */
 	void goLeft(void);
 
+	/**
+	 * Make the character go left
+	 */
 	void goRight(void);
 
+	/**
+	 * Make the character go left
+	 */
 	void stop(void);
 
 	void collide(Actor &a) override;
