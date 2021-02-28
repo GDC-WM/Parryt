c687f6ae30127ea680bbd1f541263a3df4cc80d9
Anna <amj0851@gmail.com>
2021-02-27 22:03:59 -0500

    minor changes

--- include/grunt_view.cpp
+++ include/grunt_view.cpp
@@ -9,10 +9,4 @@
 GruntView::GruntView(std::shared_ptr<LogicController> logic, std::shared_ptr<Grunt> grunt) : View(logic) {
 	this->grunt = grunt;
 	this->logic = logic;
-}
-
-
-bool GruntView::inRange(std::shared_ptr<Actor> target){
-    //do this
-    return true;
 };
