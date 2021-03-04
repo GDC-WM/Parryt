46a1518810e67e99679064793bbe191e94d19473
Aaron <amklein@email.wm.edu>
2021-03-03 21:50:23 -0500

    Cannon ai2 (#131)
    * cannon ai
    
    * Cannon range completed
    
    * Formatting and structure changes.
    
    * const.
    
    * Fixed cannon ai issues
    
    Co-authored-by: Anna <amj0851@gmail.com>

--- src/cannon_view.cpp
+++ src/cannon_view.cpp
@@ -14,22 +14,23 @@ CannonView::CannonView(std::shared_ptr<LogicController> logic, std::shared_ptr<C
 
 bool CannonView::inRange(std::shared_ptr<Actor> target) {
 	b2Vec2 targetDist = target->getBody()->GetPosition() - this->cannon->getBody()->GetPosition();
-	return sqrt((pow(targetDist.x, 2) + pow(targetDist.y, 2)))<=this->cannon->getRange();
+	return sqrt((pow(targetDist.x, 2) + pow(targetDist.y, 2))) <= this->cannon->getRange();
 }
 
 
 void CannonView::aimAt(std::shared_ptr<Actor> target) {
 	//calculate angle of target's position using trig
-	b2Vec2 targetDist = target->getBody()->GetPosition() - this->cannon->getBody()->GetPosition();
-	float targetAngle = atan2(targetDist.y, targetDist.x);
-	this->cannon->getOrientation() < targetAngle ? this->cannon->rotCounterclockwise() : this->cannon->rotClockwise();
+	b2Vec2 targetVector = target->getBody()->GetPosition() - this->cannon->getBody()->GetPosition();
+	float relativeAngle = this->cannon->getOrientation() - atan2(targetVector.y, targetVector.x);
+
+	sin(relativeAngle) < 0 ? this->cannon->rotCounterclockwise() : this->cannon->rotClockwise();
 }
 
 
 bool CannonView::updateTarget() {
 	std::list<std::shared_ptr<Actor>> actors = this->cannon->getRoom()->getActorList();
-	for (std::shared_ptr<Actor> a : actors){
-		if (a->getAllegiance() == Allegiance::PARROT && a->isTargetable() && this->inRange(a)){
+	for (std::shared_ptr<Actor> a : actors) {
+		if (a->getAllegiance() == Allegiance::PARROT && a->isTargetable() && this->inRange(a)) {
 			this->target = a;
 			return true;
 		}
