3a81e0085cb43826441f39b011a5de618eef73a4
aaronamk <amklein@email.wm.edu>
2021-03-07 00:25:46 -0500

    Switched to fully event based movement system.

--- src/character.cpp
+++ src/character.cpp
@@ -73,3 +73,26 @@ bool Character::jump(void) {
 void Character::collide(Actor &a) {
 	if (a.getAllegiance() == Allegiance::neutral && a.collides(*this)) this->jumpCounter = 0;
 }
+
+
+void Character::update(const float &dt) {
+	switch (this->movementForceDir) {
+		case Dir::left:
+			if (this->body->GetLinearVelocity().x > -this->maxSpeed)
+				this->body->ApplyForceToCenter(b2Vec2(-this->acceleration - 500, 0), true);
+			/* Refine the lookDirection */
+			if (this->body->GetLinearVelocity().x >= -this->maxSpeed && this->body->GetLinearVelocity().x < 0)
+				this->lookDir = Dir::left;
+			break;
+		case Dir::right:
+			if (this->body->GetLinearVelocity().x < this->maxSpeed) this->body->ApplyForceToCenter(b2Vec2(this->acceleration + 500, 0), true);
+				/* Refine the lookDirection */
+			if (this->body->GetLinearVelocity().x <= this->maxSpeed && this->body->GetLinearVelocity().x > 0)
+				this->lookDir = Dir::right;
+			break;
+		case Dir::none:
+			this->stop();
+			break;
+		default:; // ignore other directions
+	}
+}
