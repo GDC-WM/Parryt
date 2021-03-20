3522b60017309dfde3f1b810b672292e7d0aafa0
rlatosky <latoskyr@gmail.com>
2021-03-20 14:00:57 -0400

    Added class

---
+++ include/dialogue.hpp
@@ -0,0 +1,24 @@
+#ifndef DIALOGUE_HPP
+#define DIALOGUE_HPP
+
+#include <SFML/Graphics/Font.hpp>
+#include <SFML/Graphics/Text.hpp>
+
+class dialogue
+{
+public:
+    dialogue();
+    ~dialogue();
+private:
+    sf::Text dialogueText;
+};
+
+
+
+
+
+
+
+
+
+#endif
