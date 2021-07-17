ff64f7a67ecf1d72994befe6e251df91c838c7dc
rlatosky <latoskyr@gmail.com>
2021-07-17 15:35:09 -0400

    fixed tabs; experimental platfrom spawning.

--- src/mast.cpp
+++ src/mast.cpp
@@ -10,27 +10,27 @@
 
 Mast::Mast(b2Vec2 position, std::shared_ptr<GameState> gameState) : Actor(position) {
 
-    // set shape
-    this->mastShape.SetAsBox(this->WIDTH, this->HEIGHT);
-    this->priority = 6;
+	// set shape
+	this->mastShape.SetAsBox(this->WIDTH, this->HEIGHT);
+	this->priority = 6;
 
-    // fix shape to body
-    this->bodyDef.type = b2_staticBody;
-    this->fixtureDef.shape = &this->mastShape;
-    this->fixtureDef.density = 3.0f;
+	// fix shape to body
+	this->bodyDef.type = b2_staticBody;
+	this->fixtureDef.shape = &this->mastShape;
+	this->fixtureDef.density = 3.0f;
 	this->fixtureDef.friction = 0.7f;
 
-    // load image
-    MastTexture.loadFromFile("../resources/mast.png");
-    this->mastSprite = sf::Sprite(MastTexture);
-    
-    // set scale and origin
-    mastSprite.setScale(.20,.20);
+	// load image
+	MastTexture.loadFromFile("../resources/mast.png");
+	this->mastSprite = sf::Sprite(MastTexture);
+
+	// set scale and origin
+	mastSprite.setScale(.20,.20);
 	this->mastSprite.setOrigin(this->WIDTH, this->HEIGHT);
 
-    // add five platforms to the mast
-    auto object = std::make_shared<Platform>(b2Vec2(30,-9),1000);
-    gameState->addActor(object);
+	// add five platforms to the mast (not sure how to do...)
+	auto object = std::make_shared<Platform>(b2Vec2(this->mastSprite.getPosition().x,this->mastSprite.getPosition().y),5);
+	gameState->addActor(object);
 
 }
 // Testing; mast should not collide with player
